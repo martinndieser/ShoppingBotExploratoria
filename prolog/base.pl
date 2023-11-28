f137c0e175f1aa989aa9028641be3456fb564c88
martinndieser <133982601+martinndieser@users.noreply.github.com>
2023-11-27 22:07:15 -0300

    Add files via upload

---
+++ prolog/base.pl
@@ -0,0 +1,463 @@
+:- encoding(utf8).
+
+
+% hechos
+local("+ visión","accesorios").
+local("47 street","niños/teens").
+local("a.y. not dead","mujer/hombre").
+local("adriana costantini","mujer").
+local("akiabara","mujer").
+local("almundo.com","varios").
+local("alvarado","gastronomía").
+local("allô martinez","mujer").
+local("arredo","hogar").
+local("awada","mujer").
+local("ayres","mujer").
+local("beneida","mujer").
+local("bensimon","hombre").
+local("besha","marroquinería").
+local("broer","niños/teens").
+local("cala","mujer").
+local("caro cuore","mujer").
+local("centro rossi","servicios").
+local("ceremony","mujer/hombre").
+local("columbia","deporte").
+local("como quieres","niños/teens").
+local("complot","mujer").
+local("crêperie","gastronomía").
+local("cheeky","niños/teens").
+local("definit","servicios").
+local("delsey","varios").
+local("desiderata","mujer").
+local("elepants","mujer/hombre").
+local("estancias","mujer").
+local("eyelit","hombre").
+local("forever 21","mujer").
+local("giro didactico","niños/teens").
+local("grabatto","varios").
+local("grimoldi","marroquinería").
+local("grisino","niños/teens").
+local("havanna","gastronomía").
+local("home collection","hogar").
+local("india style","mujer").
+local("isadora","accesorios").
+local("juleriaque","varios").
+local("kosiuko","mujer/hombre").
+local("la martina","mujer/hombre").
+local("lacoste","mujer/hombre").
+local("las pepas","mujer").
+local("le pain quotidien","gastronomía").
+local("levi's","mujer/hombre").
+local("little akkiabara","niños/teens").
+local("maría cher","mujer").
+local("maría rivolta","accesorios").
+local("markova","mujer").
+local("mccafé","gastronomía").
+local("mcdonald's","gastronomía").
+local("mimo & co.","niños/teens").
+local("mishka","marroquinería").
+local("morph","hogar").
+local("mostaza","gastronomía").
+local("naíma","mujer").
+local("nespresso","varios").
+local("okko kitchen","hogar").
+local("oneclick","tecnología").
+local("opi wella","varios").
+local("osito azul","niños/teens").
+local("oxford polo club","hombre").
+local("pago fácil - western union","servicios").
+local("pandora","accesorios").
+local("paula cahen d'anvers","mujer").
+local("pelukids","servicios").
+local("penguin","hombre").
+local("portsaid","mujer").
+local("prototype","hombre").
+local("prüne","marroquinería").
+local("ramira","accesorios").
+local("rapsodia","mujer").
+local("rever pass","hombre").
+local("rochas","hombre").
+local("samsung","tecnología").
+local("selú","mujer").
+local("simmons","hogar").
+local("sweet","mujer").
+local("tienda líder","accesorios").
+local("tucci","mujer").
+local("valtik care & beauty","servicios").
+local("wanama","mujer").
+local("xl extra large","marroquinería").
+local("yenny","varios").
+local("zwass","marroquinería").
+
+nivel("+ visión","nivel uno").
+nivel("47 street","l.m. campos").
+nivel("a.y. not dead","nivel l.m. campos").
+nivel("adriana costantini","nivel arce").
+nivel("akiabara","nivel l.m. campos").
+nivel("almundo.com","nivel uno").
+nivel("alvarado","nivel uno").
+nivel("allô martinez","nivel l.m. campos").
+nivel("arredo","nivel uno").
+nivel("awada","nivel arce").
+nivel("ayres","nivel l.m. campos").
+nivel("beneida","nivel luis maría campos").
+nivel("bensimon","nivel l.m. campos").
+nivel("besha","nivel arce").
+nivel("broer","nivel uno").
+nivel("cala","nivel l.m. campos").
+nivel("caro cuore","nivel l.m. campos").
+nivel("centro rossi","nivel uno").
+nivel("ceremony","uno").
+nivel("columbia","nivel uno").
+nivel("como quieres","uno").
+nivel("complot","nivel uno").
+nivel("crêperie","nivel uno").
+nivel("cheeky","nivel l.m. campos").
+nivel("definit","nivel uno").
+nivel("delsey","nivel uno").
+nivel("desiderata","nivel l.m. campos").
+nivel("elepants","uno").
+nivel("estancias","nivel luis maría campos").
+nivel("eyelit","luis maría campos").
+nivel("forever 21","nivel luis maría campos").
+nivel("giro didactico","nivel uno").
+nivel("grabatto","nivel uno").
+nivel("grimoldi","nivel arce").
+nivel("grisino","nivel uno").
+nivel("havanna","nivel arce").
+nivel("home collection","nivel uno").
+nivel("india style","nivel l.m. campos").
+nivel("isadora","nivel uno").
+nivel("juleriaque","nivel l.m. campos").
+nivel("kio","luis maría campo").
+nivel("kosiuko","nivel l.m. campos").
+nivel("la martina","nivel l.m. campos").
+nivel("lacoste","nivel arce").
+nivel("las pepas","nivel l.m. campos").
+nivel("le pain quotidien","arce").
+nivel("levi's","nivel l.m. campos").
+nivel("little akkiabara","nivel l.m. campos").
+nivel("maría cher","nivel l.m. campos").
+nivel("maría rivolta","nivel l.m. campos").
+nivel("markova","nivel arce").
+nivel("mccafé","nivel l.m. campos").
+nivel("mcdonald's","nivel l.m. campos").
+nivel("mimo & co.","nivel l.m. campos").
+nivel("mishka","nivel l.m. campos").
+nivel("morph","nivel uno").
+nivel("mostaza","nivel uno").
+nivel("naíma","luis maría campos").
+nivel("nespresso","nivel l.m. campos").
+nivel("okko kitchen","uno").
+nivel("omint","uno").
+nivel("oneclick","nivel arce").
+nivel("opi wella","luis maría campos").
+nivel("osito azul","nivel uno").
+nivel("oxford polo club","nivel uno").
+nivel("pago fácil - western union","nivel uno").
+nivel("pandora","luis maria campos").
+nivel("paula cahen d'anvers","nivel l.m. campos").
+nivel("pelukids","nivel uno").
+nivel("penguin","nivel l.m. campos").
+nivel("portsaid","nivel l.m. campos").
+nivel("prototype","nivel l.m. campos").
+nivel("prüne","nivel arce").
+nivel("ramira","nivel l.m. campos").
+nivel("rapsodia","nivel arce").
+nivel("rever pass","nivel uno").
+nivel("rochas","nivel luis maría campos").
+nivel("samsung","nivel l.m. campos").
+nivel("selú","luís maría campos").
+nivel("simmons","nivel uno").
+nivel("sweet","nivel l.m. campos").
+nivel("tienda líder","arce").
+nivel("tucci","luis maría campos").
+nivel("valtik care & beauty","nivel uno").
+nivel("wanama","nivel l.m. campos").
+nivel("xl extra large","nivel arce").
+nivel("yenny","nivel uno").
+nivel("zwass","uno").
+
+telefono("+ visión","4778-5145").
+telefono("47 street","4778-5066").
+telefono("a.y. not dead","4778-5041").
+telefono("adriana costantini","4778-5086").
+telefono("akiabara","4778-5094").
+telefono("almundo.com","52635859").
+telefono("alvarado","4778-5195").
+telefono("allô martinez","4778-5105").
+telefono("arredo","4778-5144").
+telefono("awada","4778-5075").
+telefono("ayres","4778-5093").
+telefono("bensimon","4778-5083").
+telefono("broer","4778-5181").
+telefono("cala","11 3402-6147").
+telefono("caro cuore","4778-5065").
+telefono("centro rossi","4778-5027").
+telefono("columbia","4778-5151").
+telefono("como quieres","4778-5095").
+telefono("complot","4778-5128").
+telefono("cheeky","4778-5030").
+telefono("delsey","4778-5120").
+telefono("desiderata","4778-5149").
+telefono("grabatto","4778-5072").
+telefono("grimoldi","4778-5045").
+telefono("grisino","4778-5054").
+telefono("havanna","4778-5135").
+telefono("home collection","4778-5180").
+telefono("india style","4778-5055").
+telefono("isadora","4778-5098").
+telefono("juleriaque","4778-5171").
+telefono("kosiuko","4778-5139").
+telefono("la martina","4778-5172").
+telefono("lacoste","4778-5060").
+telefono("las pepas","4778-5076").
+telefono("le pain quotidien","4778-5062").
+telefono("levi's","4778-5039").
+telefono("little akkiabara","4778-5051 / 5183").
+telefono("maría cher","4778-5165").
+telefono("maría rivolta","4778-5099").
+telefono("mccafé","4778-5109").
+telefono("mcdonald's","4778-5115 / 4576-5810").
+telefono("mimo & co.","4778-5096").
+telefono("mishka","4778-5187").
+telefono("morph","4778-5198").
+telefono("mostaza","4778-5123").
+telefono("nespresso","4778-5026").
+telefono("oneclick","0800-345-1663").
+telefono("pago fácil - western union","4778-5084").
+telefono("pandora","4778-5032").
+telefono("paula cahen d'anvers","4778-5111").
+telefono("pelukids","47785138").
+telefono("penguin","4778-5097").
+telefono("portsaid","4778-5064").
+telefono("prototype","4778-5103").
+telefono("prüne","4778-5046").
+telefono("ramira","4778-5029").
+telefono("rapsodia","4778-5110").
+telefono("rever pass","4778-5168").
+telefono("samsung","4778-5049").
+telefono("simmons","4778-5140").
+telefono("sweet","4778-5134").
+telefono("tucci","4778-5043").
+telefono("valtik care & beauty","4778-5078").
+telefono("wanama","4778-5193").
+telefono("xl extra large","4778-5074").
+telefono("yenny","4778-5137").
+telefono("zwass","4778-5082").
+pagina_web("+ visión","www.masvision.com.ar").
+pagina_web("47 street","www.47street.com.ar").
+pagina_web("a.y. not dead","www.aynotdead.com.ar").
+pagina_web("adriana costantini","www.adrianaconstantini.com.ar").
+pagina_web("akiabara","www.akiabara.com").
+pagina_web("almundo.com","www.almundo.com.ar").
+pagina_web("alvarado","www.facebook.com/alvarado.fincadecafe/").
+pagina_web("allô martinez","www.allomartinez.com").
+pagina_web("arredo","www.arredo.com.ar").
+pagina_web("awada","www.awada.com.ar").
+pagina_web("ayres","www.ayres.com.ar").
+pagina_web("beneida","beneida.com.ar").
+pagina_web("bensimon","www.bensimon.com.ar").
+pagina_web("besha","besha.com.ar").
+pagina_web("broer","www.grisino.com/home-broer").
+pagina_web("cala","www.calandra.com.ar").
+pagina_web("caro cuore","www.carocuore.com.ar").
+pagina_web("centro rossi","www.cdrossi.com").
+pagina_web("columbia","www.columbiasportswear.com.ar").
+pagina_web("como quieres","www.comoquieres.com.ar").
+pagina_web("complot","www.complot.com.ar").
+pagina_web("cheeky","www.cheeky.com.ar").
+pagina_web("definit","definit.com.ar").
+pagina_web("delsey","www.delsey.com.ar").
+pagina_web("desiderata","www.desiderata.com.ar").
+pagina_web("elepants","www.elepants.com.ar").
+pagina_web("estancias","estanciaschiripa.com.ar").
+pagina_web("eyelit","eyelit.com.ar").
+pagina_web("giro didactico","www.girodidactico.com").
+pagina_web("grabatto","www.gbtgs.com").
+pagina_web("grimoldi","www.grimoldi.com").
+pagina_web("grisino","www.grisino.com").
+pagina_web("havanna","www.havanna.com.ar").
+pagina_web("home collection","www.homecollection.com.ar").
+pagina_web("india style","www.indiastyle.com.ar").
+pagina_web("isadora","ar.isadoraonline.com").
+pagina_web("juleriaque","www.juleriaque.com.ar").
+pagina_web("kosiuko","www.kosiuko.com").
+pagina_web("la martina","www.lamartina.com.ar").
+pagina_web("lacoste","ar.lacoste.com").
+pagina_web("las pepas","www.laspepas.com.ar").
+pagina_web("le pain quotidien","lepainquotidien.com/ar/es/").
+pagina_web("levi's","www.levi.com.ar").
+pagina_web("little akkiabara","littleakiabara.com").
+pagina_web("maría cher","www.maria-cher.com.ar").
+pagina_web("maría rivolta","www.mariarivolta.com").
+pagina_web("markova","markova.com").
+pagina_web("mccafé","www.mcdonalds.com.ar/menu/mc-cafe").
+pagina_web("mcdonald's","www.mcdonalds.com.ar").
+pagina_web("mimo & co.","www.mimo.com.ar").
+pagina_web("mishka","www.mishka.com.ar").
+pagina_web("morph","morph.com.ar").
+pagina_web("mostaza","www.mostazaweb.com.ar").
+pagina_web("naíma","naima.com.ar").
+pagina_web("nespresso","www.nespresso.com/ar/").
+pagina_web("okko kitchen","www.okko.com.ar").
+pagina_web("oneclick","www.oneclickstore.com/ar/").
+pagina_web("opi wella","https://www.opi.com").
+pagina_web("osito azul","www.ositoazul.com.ar").
+pagina_web("oxford polo club","poloclub.com.ar").
+pagina_web("pago fácil - western union","www.e-pagofacil.com").
+pagina_web("pandora","pandora.net/es-ar").
+pagina_web("paula cahen d'anvers","www.paulacahendanvers.com.ar").
+pagina_web("pelukids","pelukids.com.ar").
+pagina_web("penguin","www.penguinargentina.com").
+pagina_web("portsaid","www.portsaid.com.ar").
+pagina_web("prototype","www.prototype.com.ar").
+pagina_web("prüne","www.prune.com.ar").
+pagina_web("ramira","www.ramira.com.ar").
+pagina_web("rapsodia","www.rapsodia.com.ar").
+pagina_web("rever pass","www.reverpass.com").
+pagina_web("rochas","rochasshop.com.ar").
+pagina_web("samsung","www.samsung.com/ar/").
+pagina_web("selú","www.selu.com.ar").
+pagina_web("simmons","www.simmons.com.ar").
+pagina_web("sweet","www.sweet.com.ar").
+pagina_web("tienda líder","www.tiendalideroficial.com.ar").
+pagina_web("tucci","www.tucciweb.com").
+pagina_web("valtik care & beauty","instagram.com/valtikbeauty/").
+pagina_web("wanama","www.wanama.com").
+pagina_web("xl extra large","www.xl.com.ar").
+pagina_web("yenny","www.yenny-elateneo.com").
+
+
+categoria("accesorios").
+categoria("deporte").
+categoria("gastronomía").
+categoria("hogar").
+categoria("hombre").
+categoria("marroquinería").
+categoria("mujer").
+categoria("mujer/hombre").
+categoria("niños/teens").
+categoria("servicios").
+categoria("tecnología").
+categoria("varios").
+
+
+producto("collar", "accesorios").
+producto("lentes", "accesorios").
+producto("anillo", "accesorios").
+
+producto("pesas", "deporte").
+producto("pelota", "deporte").
+producto("raqueta", "deporte").
+
+producto("hamburguesa", "gastronomía").
+producto("comida", "gastronomía").
+producto("snacks", "gastronomía").
+
+producto("savanas", "hogar").
+producto("mueble", "hogar").
+producto("lámpara", "hogar").
+
+producto("boxer", "hombre").
+producto("remera polo", "hombre").
+producto("kit de afeitado", "hombre").
+
+producto("cartera", "marroquinería").
+producto("cinturon", "marroquinería").
+producto("cuero", "marroquinería").
+
+producto("vestido", "mujer").
+producto("bolso", "mujer").
+producto("tacos", "mujer").
+
+producto("remera", "mujer/hombre").
+producto("jean", "mujer/hombre").
+producto("pantalon chino", "mujer/hombre").
+
+producto("mochila", "niños/teens").
+producto("ropa para niño", "niños/teens").
+producto("juego de mesa", "niños/teens").
+
+producto("pagar servicios", "servicios").
+producto("masaje", "servicios").
+producto("reparación", "servicios").
+
+producto("celular", "tecnología").
+producto("auricular", "tecnología").
+producto("laptop", "tecnología").
+
+producto("termo", "varios").
+producto("cuaderno", "varios").
+producto("anillo", "varios").
+
+
+promocion(1, 'accesorios', 'Hay un 30% en cualquier negocio de la categoria de accesorios', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.vecteezy.com%2Farte-vectorial%2F2613035-sale-banner-template-new-offer-big-offers-special-offer-banner-sale-banner-template-concept&psig=AOvVaw1YbPdUdt-gNhy5DR_ZRx8p&ust=1701147966356000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOCR3pa044IDFQAAAAAdAAAAABAD').
+
+
+
+
+categorias(Categorias) :-
+	findall(Categoria, categoria(Categoria), Categorias).
+
+locales_por_categoria(Categoria, Locales) :-
+	findall(Nombre, local(Nombre, Categoria), Locales).
+
+
+%precio(zapatillas_nike, 10000).
+%precio(zapatillas_adidas, 12000).
+%precio(zapatillas_puma, 8000).
+%precio(zapatillas_reebok, 8000).
+
+
+%valoracion(zapatillas_nike, 5.0).
+%valoracion(zapatillas_adidas, 4.9).
+%valoracion(zapatillas_puma, 1).
+%valoracion(zapatillas_reebok, 4.6).
+
+
+% reglas
+
+
+%precio_mayor(x, y) :-
+%		precio(x, precio_x),
+%		precio(y, precio_y),
+%		precio_x > precio_y.
+
+%precio_menor(x, y) :-
+%	precio(x, precio_x),
+%	precio(y, precio_y),
+%	precio_x < precio_y.
+
+%precio_igual(x, y) :- not(precio_menor(x, y) ; precio_mayor(x, y)).
+
+%valoracion_mayor(x, y) :-
+%	valoracion(x, valoracion_x),
+%	valoracion(y, valoracion_y),
+%	valoracion_x > valoracion_y.
+
+%valoracion_menor(x, y) :-
+%		valoracion(x, valoracion_x),
+%		valoracion(y, valoracion_y),
+%		valoracion_x < valoracion_y.
+
+%valoracion_igual(x, y) :- not(valoracion_menor(x, y) ; valoracion_mayor(x, y)).
+
+%recomendable(x, y) :-
+%    (precio_menor(x, y) ; precio_igual(x, y)),
+%    (valoracion_mayor(x, y) ; valoracion_igual(x, y)).
+
+
+%me_alcanza(x, presupuesto) :-
+%    precio(x, precio_x),
+%    precio_x =< presupuesto.
+
+%recomendaciones_para_mi_presupuesto(presupuesto, recomendaciones) :-
+%    findall(producto, me_alcanza(producto, presupuesto), recomendaciones).
+
+%cumple_la_valoracion_minima(x, valoracion_minima) :-
+%    valoracion(x, valoracion_x),
+%    valoracion_x >= valoracion_minima.
+
+%productos_con_la_minima_valoracion(valoracion_minima, productos) :-
+%    findall(producto, cumple_la_valoracion_minima(producto, valoracion_minima), productos).
